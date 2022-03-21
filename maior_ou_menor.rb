c356cc56d82c75a90014562ca0e0480cf8d31caa
Joanice Oliveira <jhoaniceoliveira@gmail.com>
2022-03-21 14:01:31 -0300

    Adicionado random e emojis ao código

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,5 +1,8 @@
+
+numero_secreto = rand(200)
+
 puts "=================================================="
-puts "=========Jogo de Advinhação======================="
+puts "=========Jogo de Advinhação\u{1F4AD}=====================k"
 puts "=================================================="
 
 puts"=========Digite seu nome==========================="
@@ -17,7 +20,15 @@ numero = gets.chomp.to_i
 puts "Número escolhido: #{numero}"
 
 puts "Estamos comparando seu número com o número secreto!"
+puts "\u{1F7E3}\u{1F7E3}\u{1F7E3}\u{1F7E3}\u{1F7E3}\u{1F7E3}"
 sleep 5
 
-numero_secreto = 150
+
 puts "O número secreto é: #{numero_secreto}"
+
+
+if numero == numero_secreto
+    puts "Parabéns, #{nome}. Você acertou\u{1F973}"
+else
+    puts "Infelizmente, #{nome}...Você errou\u{1F622}"
+end
